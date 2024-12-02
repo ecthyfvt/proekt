b70c373451c850a5d71bd38a44584b48334a7764
ecthyfvt <zmnx6564@gmail.com>
2024-12-02 17:10:09 +0300

    updated file

--- morskoiboi.py
+++ morskoiboi.py
@@ -1,23 +1,56 @@
 import pygame
 
 
-field_size = 10
+field_width = 10
+field_height = 10
+ship_count = [4,3,2,1] # количества (n+1)-клеточных кораблей
 
-ship_count = {1:4, 2:3, 3:2, 4:1, } # количества n-клеточных кораблей
-
-
-
-
+cell_width = 40 # размер клетки
 
 
-def init_field(): # создать квадратное поле размера size
-  global field, field_size
-  field = [[0 for j in range(field_size)] for i in range(field_size)]
+
+
+def init_field(): # создать поле заданной ширины и высоты
+    global field, field_width, field_height
+    field = [['empty' for j in range(field_width)] for i in range(field_height)]
+    
+
+
+
+def place_all_ships(): # функция чтобы поставить корабли на поле
+    # надо разместить сначала самый длинный корабль
+    # потом корабль поменьше на свободное место
+    # потом еще меньше до самого маленького
+    pass
   
+  
+  
+  
+  
+def place_ship(ship_size): # поставить один корабль
+    global field
+    available_positions = [] # создаем список возможных позиций
+    # в нем будут такие параметры как положение верхней левой клетки корабля и направление корабля (вертикально или горизантально)
 
 
-def place_ships():
-  pass
+
+
+
+
+
+def render():
+    global field, cell_width
+    
+    for y, row in enumerate(field):
+        for x, cell in enumerate(row):
+            pygame.draw.rect(screen, (255,255,255), (20+30*x,20+30*y, cell_width,cell_width))
+        
+
+
+
+
+
+
 
 
 pygame.init()
@@ -27,7 +60,7 @@ clock = pygame.time.Clock()
 
 init_field() # создание игрового поля
 
-place_ships()
+place_all_ships()
 
 
 running = True
@@ -38,7 +71,6 @@ while running:
     
     clock.tick(60)
     
-    screen.fill((0, 0, 0))
-    
+    render()
     
     pygame.display.flip()
