20cd40c6b8eabf14a82261b814369d00a8a439cf
ecthyfvt <zmnx6564@gmail.com>
2024-12-02 14:23:41 +0300

    добавлены некоторые функции

--- morskoiboi.py
+++ morskoiboi.py
@@ -1,17 +1,39 @@
 import pygame
 
+
+field_size = 10
+
+ship_count = {1:4, 2:3, 3:2, 4:1, } # количества n-клеточных кораблей
+
+
+
+
+
+
+def init_field(): # создать квадратное поле размера size
+  global field, field_size
+  field = [[0 for j in range(field_size)] for i in range(field_size)]
+  
+
+
+def place_ships():
+  pass
+
+
 pygame.init()
-
 screen = pygame.display.set_mode((640, 480))
-
-
 clock = pygame.time.Clock()
 
-running = True
 
+init_field() # создание игрового поля
+
+place_ships()
+
+
+running = True
 while running:
     for event in pygame.event.get():
-        if event.type == QUIT:
+        if event.type == pygame.QUIT:
             running = False
     
     clock.tick(60)
